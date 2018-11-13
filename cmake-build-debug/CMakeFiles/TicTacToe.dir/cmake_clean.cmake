3c00ad0b82f20fb3595c31413a7b61578bd8db01
Will Fenton <wfenton@ualberta.ca>
2018-11-12 22:12:33 -0700

    Commit

---
+++ cmake-build-debug/CMakeFiles/TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToe.dir/board.cpp.o"
+  "CMakeFiles/TicTacToe.dir/main.cpp.o"
+  "CMakeFiles/TicTacToe.dir/minimax.cpp.o"
+  "CMakeFiles/TicTacToe.dir/playerFunctions.cpp.o"
+  "TicTacToe.pdb"
+  "TicTacToe.exe"
+  "libTicTacToe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
